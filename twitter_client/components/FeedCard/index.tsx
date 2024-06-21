d28e4f6b5739a624f116c2d2e53051693b156fef
AKSHAT YADAV <anshyadav56@gmail.com>
2024-06-21 19:44:25 +0530

    changes

--- twitter_client/components/FeedCard/index.tsx
+++ twitter_client/components/FeedCard/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Image from 'next/image'
 
-const FeedCard: React.FC=() => {
-    return <div>
-        
-    </div>
-}
+const FeedCard: React.FC = () => {
+    return (
+        <div className="border border-gray-600 p-4">
+            <div className="grid grid-cols-12">
+                <div className="col-span-1">
+                    <Image src="https://avatars.githubusercontent.com/u/147269291?v=4" alt="user-image" width={50} height={50}/>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default FeedCard;
